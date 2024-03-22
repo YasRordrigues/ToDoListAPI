040d1ae5aa2ae9d676a04b4d2f8e7d8626ae7707
YasRordrigues <yasno2011@live.com>
2024-03-22 20:23:51 -0300

    documentation: add logging, swagger and colletion

--- src/main/java/com/example/todolist/controllers/TaskController.java
+++ src/main/java/com/example/todolist/controllers/TaskController.java
@@ -3,8 +3,13 @@ package com.example.todolist.controllers;
 import com.example.todolist.dtos.TaskDTO;
 import com.example.todolist.entities.Task;
 import com.example.todolist.services.interfaces.ITaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,40 +21,77 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
+@Slf4j
+@Tag(name = "Task api", description = "endpoints for tasks")
 public class TaskController {
 
     private final ITaskService taskService;
 
     @PostMapping
+    @Operation(summary = "Create a new task", description = "Creates a new task and returns the created task details")
+    @ApiResponse(responseCode = "201", description = "Task successfully created")
+    @ApiResponse(responseCode = "400", description = "Bad request if the request data is invalid")
+    @ApiResponse(responseCode = "409", description = "Conflict if a task with the same details already exists")
+    @ApiResponse(responseCode = "422", description = "Unprocessable Entity if the entity is incorrect")
     public Mono<ResponseEntity<Task>> createTask(@Valid @RequestBody TaskDTO taskDTO) {
+        log.info("Creating a new task: {}", taskDTO);
         return taskService.createTask(taskDTO)
-                .map(task -> ResponseEntity.status(HttpStatus.CREATED).body(task));
+                .map(task -> {
+                    log.info("Task created: {}", task);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(task);
+                });
     }
 
     @GetMapping
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of task list", content = @Content)
+    @ApiResponse(responseCode = "204", description = "No tasks available")
     public Mono<ResponseEntity<Flux<Task>>> getAllTasks() {
+        log.info("Requested to get all tasks");
         Flux<Task> tasks = taskService.getAllTasks();
-        return tasks.collectList()
-                .flatMap(list -> list.isEmpty()
-                        ? Mono.just(ResponseEntity.noContent().build())
-                        : Mono.just(ResponseEntity.ok(tasks)));
+        return tasks.collectList().map(list ->
+                list.isEmpty() ?
+                        new ResponseEntity<>(null, HttpStatus.NO_CONTENT) :
+                        new ResponseEntity<>(tasks, HttpStatus.OK)
+        );
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find task by ID", description = "Returns the task details for a given ID")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of task details", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Task not found")
     public Mono<ResponseEntity<Task>> getTaskById(@PathVariable UUID id) {
+        log.info("Requested to get task by ID: {}", id);
         return taskService.getTaskById(id)
-                .map(ResponseEntity::ok)
+                .map(task -> {
+                    log.info("Task found: {}", task);
+                    return ResponseEntity.ok(task);
+                })
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a task", description = "Updates a task and returns the updated task details")
+    @ApiResponse(responseCode = "200", description = "Task successfully updated")
+    @ApiResponse(responseCode = "400", description = "Bad request if the request data is invalid")
+    @ApiResponse(responseCode = "404", description = "Not Found if the task with the specified ID does not exist")
+    @ApiResponse(responseCode = "409", description = "Conflict if a task with the same details already exists")
+    @ApiResponse(responseCode = "422", description = "Unprocessable Entity if the entity is incorrect")
     public Mono<ResponseEntity<Task>> updateTask(@PathVariable UUID id, @Valid @RequestBody TaskDTO taskDTO) {
+        log.info("Updating task with ID: {} with data: {}", id, taskDTO);
         return taskService.updateTask(id, taskDTO)
-                .map(ResponseEntity::ok);
+                .map(updatedTask -> {
+                    log.info("Task updated: {}", updatedTask);
+                    return ResponseEntity.ok(updatedTask);
+                });
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a task", description = "Deletes a task based on the given ID")
+    @ApiResponse(responseCode = "204", description = "Task successfully deleted")
+    @ApiResponse(responseCode = "400", description = "Bad request if the deletion cannot be performed")
+    @ApiResponse(responseCode = "404", description = "Not Found if the task with the specified ID does not exist")
     public Mono<ResponseEntity<Void>> deleteTask(@PathVariable UUID id) {
+        log.info("Deleting task with ID: {}", id);
         return taskService.deleteTask(id)
                 .then(Mono.just(new ResponseEntity<>(HttpStatus.NO_CONTENT)));
     }
--- src/main/java/com/example/todolist/services/TaskService.java
+++ src/main/java/com/example/todolist/services/TaskService.java
@@ -8,6 +8,7 @@ import com.example.todolist.errors.TaskValidationException;
 import com.example.todolist.repositories.TaskRepository;
 import com.example.todolist.services.interfaces.ITaskService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -18,6 +19,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TaskService implements ITaskService {
 
     private final TaskRepository taskRepository;
@@ -25,53 +27,89 @@ public class TaskService implements ITaskService {
 
     @Override
     public Mono<Task> createTask(TaskDTO taskDTO) {
+        log.info("Attempting to create task: {}", taskDTO);
         LocalDateTime now = LocalDateTime.now();
         if (taskDTO.getExpirationDate().isBefore(now)) {
+            log.error("Failed to create task, expiration date must be in the future.");
             return Mono.error(new TaskValidationException("Expiration date must be in the future."));
         }
         return this.taskRepository.findByTitle(taskDTO.getTitle())
-                .flatMap(dbTask -> Mono.<Task>error(new TaskTitleExistsException("A task with the given title already exists.")))
+                .flatMap(dbTask -> {
+                    log.error("Failed to create task, a task with the given title '{}' already exists.", taskDTO.getTitle());
+                    return Mono.error(new TaskTitleExistsException("A task with the given title already exists."));
+                })
                 .switchIfEmpty(Mono.defer(() -> {
                     Task task = modelMapper.map(taskDTO, Task.class);
                     task.setCreationDate(now);
+                    log.info("Task with title '{}' passed validation and will be saved.", task.getTitle());
                     return this.taskRepository.save(task);
-                }));
+                }))
+                .cast(Task.class)
+                .doOnSuccess(createdTask -> {
+                    if (createdTask != null) {
+                        log.info("Task '{}' created successfully.", createdTask.getTitle());
+                    }
+                })
+                .doOnError(e -> log.error("Task creation failed due to an exception: {}", e.getMessage()));
     }
+
+
 
     @Override
     public Flux<Task> getAllTasks() {
-        return taskRepository.findAll();
+        log.info("Fetching all tasks.");
+        return taskRepository.findAll()
+                .doOnComplete(() -> log.info("All tasks fetched successfully."))
+                .doOnError(e -> log.error("Error fetching tasks: {}", e.getMessage()));
     }
 
     @Override
     public Mono<Task> getTaskById(UUID id) {
+        log.info("Looking for task with id '{}'.", id);
         return taskRepository.findById(id)
-                .switchIfEmpty(Mono.error(new TaskNotFoundException("Task not found with id: " + id)));
+                .doOnSuccess(task -> log.info("Task with id '{}' found.", task.getId()))
+                .switchIfEmpty(Mono.defer(() -> {
+                    log.warn("Task with id '{}' not found.", id);
+                    return Mono.error(new TaskNotFoundException("Task not found with id: " + id));
+                }));
     }
 
     @Override
     public Mono<Task> updateTask(UUID id, TaskDTO taskDetails) {
+        log.info("Attempting to update task with id '{}'.", id);
         return taskRepository.findById(id)
-                .switchIfEmpty(Mono.error(new TaskNotFoundException("Task not found with id: " + id)))
-
+                .switchIfEmpty(Mono.defer(() -> {
+                    log.error("Update failed, task with id '{}' not found.", id);
+                    return Mono.error(new TaskNotFoundException("Task not found with id: " + id));
+                }))
                 .flatMap(existingTask -> {
                     if (taskDetails.getExpirationDate().isBefore(existingTask.getCreationDate())) {
+                        log.error("Update failed, expiration date cannot be before creation date for task '{}'.", existingTask.getId());
                         return Mono.error(new TaskValidationException("Expiration date cannot be before creation date."));
                     }
+                    log.info("Updating task '{}'.", existingTask.getId());
                     existingTask.setTitle(taskDetails.getTitle());
                     existingTask.setDescription(taskDetails.getDescription());
                     existingTask.setExpirationDate(taskDetails.getExpirationDate());
                     return taskRepository.save(existingTask);
-                });
+                })
+                .doOnSuccess(updatedTask -> log.info("Task with id '{}' updated successfully.", updatedTask.getId()))
+                .doOnError(e -> log.error("Update failed for task with id '{}': {}", id, e.getMessage()));
     }
-
 
     @Override
     public Mono<Void> deleteTask(UUID id) {
+        log.info("Attempting to delete task with id '{}'.", id);
         return taskRepository.findById(id)
-                .switchIfEmpty(Mono.error(new TaskNotFoundException("Task not found with id: " + id)))
-                .flatMap(task -> taskRepository.deleteById(id));
+                .switchIfEmpty(Mono.defer(() -> {
+                    log.error("Delete failed, task with id '{}' not found.", id);
+                    return Mono.error(new TaskNotFoundException("Task not found with id: " + id));
+                }))
+                .flatMap(task -> {
+                    log.info("Task with id '{}' will be deleted.", task.getId());
+                    return taskRepository.deleteById(id);
+                })
+                .doOnSuccess(aVoid -> log.info("Task with id '{}' deleted successfully.", id))
+                .doOnError(e -> log.error("Delete failed for task with id '{}': {}", id, e.getMessage()));
     }
-
-
 }
